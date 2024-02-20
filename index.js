1777b967ddd0b95be28e3d22451d870fdbfc3843
DavidLukasMelo <160607282+DavidLukasMelo@users.noreply.github.com>
2024-02-20 15:08:32 +0000

    reqres

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello there")
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/oi', function(req, res) {
+    res.send('hello there')
+})
+
+app.listen(3000)
