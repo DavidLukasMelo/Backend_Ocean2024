87231e5414d4a0aa5037492a56f4350de41890af
DavidLukasMelo <160607282+DavidLukasMelo@users.noreply.github.com>
2024-02-21 16:11:06 +0000

    Index.js upgradeloko

--- index.js
+++ index.js
@@ -9,4 +9,39 @@ app.get('/oi', function(req, res) {
     res.send('hello there')
 })
 
+//  Lista de Nomes
+
+const list = ["A1", "A2", "A3"]
+
+// Read All --> GET
+app.get('/item', function(req, res){
+   res.send(list)
+})
+app.get('/item/:id', function(req, res) {
+  //Acesso id no parametro de rota
+  const id = req.params.id
+  //Acesso item na lista baseado no id
+  const item = list[id]
+  //enviando o item como resposta
+  res.send(item)
+})
+
+//sinalizando que o corpo da req e JSON
+app.use(express.json())
+
+//Creat POST
+app.post('/item', function(req, res) {
+  //Extraindo o corpo da req
+  const body = req.body
+
+  //pegando o nome que foi enviado
+  const item = body.nome
+
+  //colocando nome dentro da lista item
+  list.push(item)
+
+  //enviando uma resposta
+  res.send('item adicionado')
+})
+
 app.listen(3000)
