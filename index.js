f03bb863e5a408a8297dfafb6e75348fa993a2fe
DavidLukasMelo <160607282+DavidLukasMelo@users.noreply.github.com>
2024-02-27 14:57:39 +0000

    lokuraMongoDb

--- index.js
+++ index.js
@@ -1,47 +1,67 @@
 const express = require('express')
-const app = express()
+const { MongoClient } = require('mongodb')
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const dbUrl = 'mongodb+srv://admin:hhtAGKhZbbByByaR@cluster0.zhgulcw.mongodb.net/'
+const dbName = 'Ocean2024'
 
-app.get('/oi', function(req, res) {
+
+
+async function main() {
+
+  const client = new MongoClient(dbUrl)
+
+  console.log('Conectando na lokura...')
+  await client.connect()
+  console.log('Sucesso meu nobre!')
+
+
+  const app = express()
+
+  app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+
+  app.get('/oi', function (req, res) {
     res.send('hello there')
-})
+  })
 
-//  Lista de Nomes
+  //  Lista de Nomes
 
-const list = ["A1", "A2", "A3"]
+  const list = ["A1", "A2", "A3"]
 
-// Read All --> GET
-app.get('/item', function(req, res){
-   res.send(list)
-})
-app.get('/item/:id', function(req, res) {
-  //Acesso id no parametro de rota
-  const id = req.params.id
-  //Acesso item na lista baseado no id
-  const item = list[id]
-  //enviando o item como resposta
-  res.send(item)
-})
+  // Read All --> GET
+  app.get('/item', function (req, res) {
+    res.send(list)
+  })
+  app.get('/item/:id', function (req, res) {
+    //Acesso id no parametro de rota
+    const id = req.params.id
+    //Acesso item na lista baseado no id
+    const item = list[id]
+    //enviando o item como resposta
+    res.send(item)
+  })
 
-//sinalizando que o corpo da req e JSON
-app.use(express.json())
+  //sinalizando que o corpo da req e JSON
+  app.use(express.json())
 
-//Creat POST
-app.post('/item', function(req, res) {
-  //Extraindo o corpo da req
-  const body = req.body
+  //Creat POST
+  app.post('/item', function (req, res) {
+    //Extraindo o corpo da req
+    const body = req.body
 
-  //pegando o nome que foi enviado
-  const item = body.nome
+    //pegando o nome que foi enviado
+    const item = body.nome
 
-  //colocando nome dentro da lista item
-  list.push(item)
+    //colocando nome dentro da lista item
+    list.push(item)
 
-  //enviando uma resposta
-  res.send('item adicionado')
-})
+    //enviando uma resposta
+    res.send('item adicionado')
+  })
 
-app.listen(3000)
+  app.listen(3000)
+
+}
+
+main()
